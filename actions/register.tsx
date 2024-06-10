b59f571d7421dba941e466550b4532c2b41f5925
Alisher Kadraliev <alisherkadraliev@gmail.com>
2024-06-10 22:11:30 +0300

    added db connection

--- actions/register.tsx
+++ actions/register.tsx
@@ -1,12 +1,21 @@
 "use server"
+
+import bcrypt from "bcrypt"
+// import {db} from "@/lib/db"
 import * as z from "zod"
 import { RegisterSchema } from "@/schemas"
 
-export const register = async(values: z.infer<typeof RegisterSchema>) => {
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values)
 
     if (!validatedFields.success) {
-        return {error:'Invalid Field!'}
+        return { error: 'Invalid Field!' }
     }
-    return {success:"Register success and Email sended!"}
+
+    const { email, password, name } = validatedFields.data
+    const hashedPassword = await bcrypt.hash(password, 10)
+    
+
+
+    return { success: "Register success and Email sended!" }
 }
